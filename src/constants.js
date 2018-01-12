51dfb7c1c8bc6eccd36905a3f947f9aaf6cae088
chai <chai.chai1222@gmail.com>
2018-01-12 20:06:48 +0900

    経県値用に修正してダミーデータで表示する

---
+++ src/constants.js
@@ -0,0 +1,40 @@
+// 定数定義
+
+export const EXPERIENCES = [
+    { // 0
+        name: 'unexplored',
+        mark: '×',
+        text: '未踏',
+        subtext: '未経県'
+    },
+    { // 1
+        name: 'passage',
+        mark: '▲',
+        text: '通過',
+        subtext: '通過した'
+    },
+    { // 2
+        name: 'earthing',
+        mark: '△',
+        text: '接地',
+        subtext: '降り立った'
+    },
+    { // 3
+        name: 'visit',
+        mark: '●',
+        text: '訪問',
+        subtext: '歩いた'
+    },
+    { // 4
+        name: 'stay',
+        mark: '○',
+        text: '宿泊',
+        subtext: '泊まった'
+    },
+    { // 5
+        name: 'live',
+        mark: '◎',
+        text: '居住',
+        subtext: '住んだ'
+    },
+]
