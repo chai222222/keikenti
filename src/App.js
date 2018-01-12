2c1382c37cd5612aa80299caa1be78c39b16aabc
chai <chai.chai1222@gmail.com>
2018-01-12 20:28:08 +0900

    経県値用に修正してダミーデータで表示する

--- src/App.js
+++ src/App.js
@@ -3,7 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 import Map from './Map';
 
+import { EXPERIENCES } from './constants';
+
+// geo2topo output
+import japanMap from './japan_geo2topo.json';
+
 class App extends Component {
+
+  constructor() {
+    super();
+    const dummy = Array.from(Array(japanMap.objects['-'].geometries.length))
+      .map(d => Math.floor(Math.random() * EXPERIENCES.length) + '').join('');
+    this.state = this._createState(dummy);
+  }
+
+  _createState(prefStateStr = '', name = '', title = '') {
+    const st = {
+      prefNames: null, // 北海道～沖縄までのID順の配列
+      name, // your name
+      title, // title
+      prefData: { // nameをキーにした状態Map
+      }
+    };
+    st.prefNames = japanMap.objects['-'].geometries
+      .sort((a, b) => b.properties.iso_3166_2.substr(2) - a.properties.iso_3166_2.substr(2) ) // 'ID-xx' の xx の数値でソート
+      .map((pref, idx) => {
+        const name = pref.properties.name;
+        st.prefData[name] = {
+          id: pref.properties.iso_3166_2,
+          local: pref.properties.name_local,
+          experience: this._extractExperience(prefStateStr, idx)
+        };
+        return name;
+      });
+    return st;
+  }
+
+  _extractExperience(prefStateStr, idx) {
+    const plen = prefStateStr.length;
+    const e = idx < plen ? prefStateStr.substr(idx, 1) : '0';
+    return (e - 0) % EXPERIENCES.length;
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,7 +55,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Map />
+        <Map width="1300" height="960" scale="2000" mapState={{...this.state}} />
       </div>
     );
   }
--- src/Map.js
+++ src/Map.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import * as topojson from 'topojson-client';
 import * as d3 from 'd3';
 
 // geo2topo output
 import mapJson from './japan_geo2topo.json';
+import { EXPERIENCES } from './constants';
 
 class Map extends Component {
 
+  static propTypes = {
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+    scale: PropTypes.number.isRequired,
+    mapState: PropTypes.object.isRequired,
+  }; 
   componentDidMount() {
     this.drawMap()
   }
@@ -17,37 +25,48 @@ class Map extends Component {
 
   drawMap() {
 
-    var w = 1300;
-    var h = 960;
-    // 地図の投影図法を設定する．
-    var projection = d3.geoMercator()
-        .center([136, 35.5])
-        .scale(2000)
-        .translate([w / 2, h / 2]);
-
-    // GeoJSONからpath要素を作る．
-    var path = d3.geoPath()
-        .projection(projection);
+    const w = this.props.width;
+    const h = this.props.height;
 
     const jpn = mapJson;
     const geoJp = topojson.feature(jpn, jpn.objects['-']);
+
+    const center = d3.geoCentroid(geoJp);
+
+    // 地図の投影図法を設定する．
+    var projection = d3.geoMercator()
+        .center(center)
+        .scale(this.props.scale)
+        .translate([w / 2, h / 2]);
+
+    // GeoJSONからpath要素を作る．
+    var path = d3.geoPath().projection(projection);
+
     console.log(geoJp);
     const svg = d3.select(this.node);
+    svg.append('rect') // うーみ
+      .attr('x', 0)
+      .attr('y', 0)
+      .attr('width', w)
+      .attr('height', h)
+      .attr('style', 'fill: #3987c9;');
     svg.attr('height', h)
       .attr('width', w)
       .selectAll("path")
       .data(geoJp.features)
       .enter()
       .append("path")
-      .attr("class", d => d.properties.region ? `region ${d.properties.region.toLowerCase()}` : 'region')
-      .attr("d", path);
+        .attr("class", d => this._name2Class(d.properties.name))
+        .attr("d", path);
+  }
+
+  _name2Class(name) {
+    console.log(this.props.mapState.prefData[name].experience);
+    return EXPERIENCES[this.props.mapState.prefData[name].experience].name;
   }
 
   render() {
-    return (
-      <svg ref={node => this.node = node}>
-      </svg>
-    );
+    return <svg ref={node => this.node = node} />
   }
 }
 
